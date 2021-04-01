6d4dc1b77c374ee75d5eb1549a70e4eeff714278
Glitch (patch-fantastic-tibia) <none>
2021-04-01 07:49:10 +0000

    🎅💇 Updated with Glitch

---
+++ server.js
@@ -0,0 +1,31 @@
+var dbd = require("dbd.js");
+var fs = require("fs");
+
+const bot = new dbd.Bot({
+  token: "ODI3MDc1NzY5NzgzNTQ5OTgy.YGVwVQ.vTKA8O8Tgzep3oltqewsbBjAodw",
+  prefix: "$getServerVar[prefix]"
+});
+
+bot.status({
+  text: "/help",
+  type: "LISTENING",
+  time: 12
+});
+
+bot.onMessage();
+
+// Variable
+bot.variables({
+  prefix: "/",
+  warn: "0"
+});
+
+//commands handler
+var reader = fs.readdirSync("./commands/").filter(file => file.endsWith(".js"));
+for (const file of reader) {
+  const command = require(`./commands/${file}`);
+  bot.command({
+    name: command.name,
+    code: command.code
+  });
+}
